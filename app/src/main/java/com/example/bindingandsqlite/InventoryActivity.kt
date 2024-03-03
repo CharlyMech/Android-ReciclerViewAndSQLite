d7b928ffa54b4246ce8fe2778050b0375b19bffe
CharlyMech <sanchezreciocarlos99@gmail.cim>
2024-03-03 11:34:24 +0100

    FIX Toolbar now implements back arrow

--- app/src/main/java/com/example/bindingandsqlite/InventoryActivity.kt
+++ app/src/main/java/com/example/bindingandsqlite/InventoryActivity.kt
@@ -2,10 +2,20 @@ package com.example.bindingandsqlite
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class InventoryActivity : AppCompatActivity() {
+	// Class variables
+	private lateinit var toolbar: Toolbar
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_inventory)
+
+		// Get all elements from activity layout file
+		this.toolbar = findViewById(R.id.toolbar)
+
+		// Set ActionBar properties
+		setSupportActionBar(this.toolbar)
+		supportActionBar!!.setDisplayHomeAsUpEnabled(true)
 	}
 }
